ecddd150785589eeb41c4356d82115f65c831058
Sean Aguilar <seanaguilar698@gmail.com>
2024-09-05 21:13:09 +0800

    add modular label

--- sum_of_array.asm
+++ sum_of_array.asm
@@ -1,4 +1,3 @@
-
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
@@ -14,11 +13,8 @@ org 100h
     
     .DISPLAY_WORD:                  ; DISPLAYS WORD 
         MOV DL, [SI]
-        MOV DH, 00001011B   
-        MOV ES:[DI], DX
+        CALL .DISPLAY
         INC SI
-        INC DI
-        INC DI
     LOOP .DISPLAY_WORD
     
     LEA SI, NUMS                    ; LOAD NUMS DB INTO SI
@@ -28,11 +24,8 @@ org 100h
         MOV DL, [SI]
         ADD BL, DL
         ADD DL, '0'
-        MOV DH, 00001011B   
-        MOV ES:[DI], DX
+        CALL .DISPLAY
         INC SI
-        INC DI
-        INC DI
         
         DEC CX                      ; DECREMENTS CX AFTER A CHAR IS DISPLAYED
         JZ .DISPLAY_EQUALS          ; DISPLAYS EQUAL SIGN WHEN CX IS 0
@@ -44,40 +37,35 @@ org 100h
     .DISPLAY_EQUALS:                ; DISPLAYS EQUAL SIGN WITH SPACE
         CALL .SPACE
         MOV DL, '='
-        MOV DH, 00001011B
-        MOV ES:[DI], DX
-        INC DI
-        INC DI
+        CALL .DISPLAY
         CALL .SPACE
         
     .DISPLAY_SUM:                   ; DISPLAY THE SUM OF NUMS (SUPPORTS 0-9 ONLY)
         MOV DL, BL
         ADD DL, '0'
-        MOV DH, 00001011B
-        MOV ES:[DI], DX
+        CALL .DISPLAY
         INC SI
-        INC DI
-        INC DI
         JMP .END       
     
     .PLUS:                          ; DISPLAYS PLUS SIGN
         MOV DL, '+'
-        MOV DH, 00001011B
-        MOV ES:[DI], DX
-        INC DI
-        INC DI
+        CALL .DISPLAY
         RET
         
     .SPACE:                         ; DISPLAYS WHITE SPACE
         MOV DL, ' '
+        CALL .DISPLAY
+        RET
+    
+    .DISPLAY:   
         MOV DH, 00001011B
         MOV ES:[DI], DX
         INC DI
         INC DI
-        RET     
+        RET          
 
 .END:
 ret
   
 WORD DB 'THE SUM OF '
-NUMS DB 1, 2, 3, 2, 1
+NUMS DB 11, 2, 3, 2, 1
