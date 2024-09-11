3eba60ddf58271163186088249b0ec78f780ba52
Sean Aguilar <seanaguilar698@gmail.com>
2024-09-11 12:31:41 +0800

    Add division function

--- sum_of_array.asm
+++ sum_of_array.asm
@@ -18,7 +18,7 @@ MAIN PROC
         INC SI
     LOOP .DISPLAY_WORD
     
-    LEA SI, NUMs                        ; LOAD WORD DB INTO SI
+    LEA SI, NUMS                        ; LOAD WORD DB INTO SI
     MOV CX, 5                           ; LENGHT OF NUM ARRAY
     
     XOR BX, BX
@@ -57,29 +57,58 @@ MAIN PROC
         XOR BX, BX
         
         .DISPLAY_SUM:                   ; DISPLAYS NUM
-            MOV BL, 10                  ; MOV 10 TO FOR DIVISION
-            DIV BL                      ; AX/BL, STORE QUOTIENT TO AL & REMAINDER TO AH
-            MOV BH, AH
-            MOV AH, 0H
-            DIV BL 
-                  
-            MOV DL, AL                  ; MOVE HUNDREDS TO DL
-            ADD DL, '0'      
-            CALL DISPLAY_CHAR
-            
-            MOV DL, AH                  ; MOVE TENS TO DL
-            ADD DL, '0'      
-            CALL DISPLAY_CHAR
-            
-            MOV DL, BH                  ; MOVE ONES TO DL
-            ADD DL, '0'      
-            CALL DISPLAY_CHAR
-            
-            MOV DL, ' '
-            CALL DISPLAY_CHAR
+            CALL DISPLAY_3DIGIT
             INC SI
         LOOP .DISPLAY_SUM
-        JMP .END
+        
+        
+    MOV DI, 160    
+    LEA SI, WORD2                        ; LOAD WORD DB INTO SI
+    MOV CX, 11                          ; LENGHT OF WORD
+    
+    .DISPLAY_WORD2:                      ; DISPLAYS WORD 
+        MOV DL, [SI]
+        CALL DISPLAY_CHAR
+        INC SI
+    LOOP .DISPLAY_WORD2
+    
+    CALL GET_QUOTIENT
+    
+    LEA SI, QUOTIENT
+    MOV CX, 1
+    
+    XOR AX, AX
+    MOV AL, QUOTIENT
+    XOR BX, BX
+    
+    .DISPLAY_QUOTIENT:
+        CALL  DISPLAY_2DIGIT
+        INC SI
+    LOOP .DISPLAY_QUOTIENT  
+    
+    MOV DI, 320    
+    LEA SI, WORD3                        ; LOAD WORD DB INTO SI
+    MOV CX, 12                          ; LENGHT OF WORD
+    
+    .DISPLAY_WORD3:                      ; DISPLAYS WORD 
+        MOV DL, [SI]
+        CALL DISPLAY_CHAR
+        INC SI
+    LOOP .DISPLAY_WORD3
+    
+    LEA SI, REMAINDER
+    MOV AL, REMAINDER
+    MOV CX, 1
+    
+    XOR BX, BX
+    
+    .DISPLAY_REMAINDER:   
+        MOV DL, AL
+        ADD DL, '0'
+        CALL DISPLAY_CHAR  
+        INC SI
+    LOOP .DISPLAY_REMAINDER 
+          
 .END:           
     ret
 MAIN ENDP
@@ -101,7 +130,62 @@ DISPLAY_EQUAL PROC                      ; SUB ROUTINE TO DISPLAY EQUAL SIGN
     CALL DISPLAY_CHAR
     JMP .READY_SUM
 DISPLAY_EQUAL ENDP
+
+DISPLAY_2DIGIT PROC
+    MOV AX, 00H
+    MOV AL, [SI]
+    MOV BL, 10                      ; MOV 10 TO FOR DIVISION
+    DIV BL                          ; AX/BL, STORE QUOTIENT TO AL & REMAINDER TO AH
+      
+    MOV DL, AL                      ; MOVE TENS TO DL
+    ADD DL, '0'      
+    CALL DISPLAY_CHAR
+    
+    MOV DL, AH                      ; MOVE ONES TO DL
+    ADD DL, '0'      
+    CALL DISPLAY_CHAR           
+    RET
+DISPLAY_2DIGIT ENDP    
+
+DISPLAY_3DIGIT PROC
+    MOV BL, 10                  ; MOV 10 TO FOR DIVISION
+    DIV BL                      ; AX/BL, STORE QUOTIENT TO AL & REMAINDER TO AH
+    MOV BH, AH
+    MOV AH, 0H
+    DIV BL 
+      
+    MOV DL, AL                  ; MOVE HUNDREDS TO DL
+    ADD DL, '0'      
+    CALL DISPLAY_CHAR
+    
+    MOV DL, AH                  ; MOVE TENS TO DL
+    ADD DL, '0'      
+    CALL DISPLAY_CHAR
+    
+    MOV DL, BH                  ; MOVE ONES TO DL
+    ADD DL, '0'      
+    CALL DISPLAY_CHAR
+    
+    MOV DL, ' '
+    CALL DISPLAY_CHAR
+    RET
+DISPLAY_3DIGIT ENDP    
+
+GET_QUOTIENT PROC
+    MOV AX, SUM     
+    MOV BX, 00H
+    MOV BL, DVSR
+    DIV BL
+    MOV QUOTIENT, AL
+    MOV REMAINDER, AH
+    RET
+GET_QUOTIENT ENDP
   
 WORD DB 'THE SUM OF '
-NUMS DB 55, 55, 55, 55, 55
-SUM DW 0
+WORD2 DB 'QUOTIENT = '
+WORD3 DB 'REMAINDER = '
+NUMS DB 12, 23, 34, 45, 56
+SUM DW ?
+QUOTIENT DB ?
+REMAINDER DB ?                  
+DVSR DB 03
