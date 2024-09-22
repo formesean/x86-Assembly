95a7b74457997b7af16438566c0da0bce7433733
Sean Aguilar <seanaguilar698@gmail.com>
2024-09-22 22:13:30 +0800

    Update PME1.asm

--- PME1.asm
+++ PME1.asm
@@ -22,7 +22,9 @@ org 100h
         MOV BH, INPUT[[1]
         CMP BH, 5
         JE LEN5
-        
+        CMP BH, 7
+        JE LEN7
+               
         MOV BH, 0  
         MOV DL, 41
         SUB DL, INPUT[1]
@@ -54,6 +56,8 @@ AT_TOP PROC
     MOV BL, INPUT[1]
     CMP BX, 5
     JE MOVE_2OUTWARDS
+    CMP BX, 7
+    JE MOVE_3OUTWARDS
     
     MOV CX, 38    
     MOV DI, 76
@@ -104,6 +108,12 @@ AT_TOP ENDP
 LEN5:  
     MOV BH, 0  
     MOV DL, 43
+    SUB DL, INPUT[1]    
+    JMP START
+
+LEN7:  
+    MOV BH, 0  
+    MOV DL, 44
     SUB DL, INPUT[1]    
     JMP START
 
@@ -184,8 +194,8 @@ MOVE_2OUTWARDS:
         PUSH DI
         PUSH SI
         
-        MOV DI, L4_HOLDER1
-        MOV SI, L5_HOLDER1
+        MOV DI, LEN5_L4_HOLDER1[0]
+        MOV SI, LEN5_L5_HOLDER1[0]
 
         MOV DL, INPUT[5]
         MOV DH, 00H
@@ -203,8 +213,8 @@ MOVE_2OUTWARDS:
         MOV BH, 07H
         MOV ES:[SI], BX
         
-        MOV L4_HOLDER1, DI
-        MOV L5_HOLDER1, SI        
+        MOV LEN5_L4_HOLDER1[0], DI
+        MOV LEN5_L5_HOLDER1[0], SI        
         LOOP MOVE_2
     
     DONE2:
@@ -218,6 +228,120 @@ MOVE_2OUTWARDS:
         CALL MOVE_DOWN 
        
         JMP MOVE_2INWARDS  
+
+MOVE_3OUTWARDS:
+    MOV CX, 36 
+    MOV DI, 74
+    MOV SI, 76
+    MOV BP, 78
+    ; 72 76 78 80 82 84 86
+
+    MOV DL, INPUT[2]
+    MOV DH, 00H
+    MOV ES:[DI], DX
+    MOV BL, INPUT[3]
+    MOV BH, 00H
+    MOV ES:[SI], BX
+    MOV AL, INPUT[4]
+    MOV AH, 00H
+    MOV ES:[BP], AX
+    
+    SUB DI, 2
+    MOV DL, INPUT[2]
+    MOV DH, 07H
+    MOV ES:[DI], DX    
+    SUB SI, 2
+    MOV BL, INPUT[3]
+    MOV BH, 07H
+    MOV ES:[SI], BX
+    SUB BP, 2
+    MOV BL, INPUT[4]
+    MOV BH, 07H
+    MOV ES:[BP], BX
+    
+    PUSH DI
+    PUSH SI
+    PUSH BP
+
+    MOVE_3:
+        ;WL_7 DW 76, 78, 80, 82, 84, 86, 88    
+        CMP CX, 0
+        JE DONE4
+        
+        POP BP
+        POP SI
+        POP DI
+        
+        MOV DL, INPUT[2]
+        MOV DH, 00H
+        MOV ES:[DI], DX
+        MOV BL, INPUT[3]
+        MOV BH, 00H
+        MOV ES:[SI], BX
+        MOV AL, INPUT[4]
+        MOV AH, 00H
+        MOV ES:[BP], AX
+            
+        SUB DI, 2
+        MOV DL, INPUT[2]
+        MOV DH, 07H
+        MOV ES:[DI], DX    
+        SUB SI, 2
+        MOV BL, INPUT[3]
+        MOV BH, 07H
+        MOV ES:[SI], BX
+        SUB BP, 2
+        MOV AL, INPUT[4]
+        MOV AH, 07H
+        MOV ES:[BP], AX
+        
+        PUSH DI
+        PUSH SI
+        PUSH BP
+        
+        MOV DI, LEN7_L5_HOLDER1
+        MOV SI, LEN7_L6_HOLDER1
+        MOV BP, LEN7_L7_HOLDER1
+
+        MOV DL, INPUT[6]
+        MOV DH, 00H
+        MOV ES:[DI], DX
+        MOV BL, INPUT[7]
+        MOV BH, 00H
+        MOV ES:[SI], BX
+        MOV AL, INPUT[8]
+        MOV AH, 00H
+        MOV ES:[BP], BX
+        
+        ADD DI, 2
+        MOV DL, INPUT[6]
+        MOV DH, 07H
+        MOV ES:[DI], DX    
+        ADD SI, 2
+        MOV BL, INPUT[7]
+        MOV BH, 07H
+        MOV ES:[SI], BX
+        ADD BP, 2
+        MOV AL, INPUT[8]
+        MOV AH, 07H
+        MOV ES:[BP], AX
+        
+        MOV LEN7_L5_HOLDER1, DI
+        MOV LEN7_L6_HOLDER1, SI
+        MOV LEN7_L7_HOLDER1, BP        
+        LOOP MOVE_3
+    
+    DONE4:
+        CALL MOVE_DOWN 
+        CALL MOVE_DOWN
+        CALL MOVE_DOWN
+        CALL MOVE_DOWN
+        CALL MOVE_DOWN 
+        CALL MOVE_DOWN
+        CALL MOVE_DOWN
+        CALL MOVE_DOWN
+    
+    JMP MOVE_3INWARDS
 
 MOVE_DOWN: 
     MOV AL, 1
@@ -300,7 +424,7 @@ MOVE_2INWARDS:
 
     MOVE_21:    
         CMP CX, 0
-        JE DONE3
+        JE DONE5
         
         POP SI
         POP DI
@@ -324,8 +448,8 @@ MOVE_2INWARDS:
         PUSH DI
         PUSH SI
         
-        MOV DI, L4_HOLDER2
-        MOV SI, L5_HOLDER2
+        MOV DI, LEN5_L4_HOLDER2[0]
+        MOV SI, LEN5_L5_HOLDER2[0]
 
         MOV DL, INPUT[5]
         MOV DH, 00H
@@ -343,11 +467,122 @@ MOVE_2INWARDS:
         MOV BH, 07H
         MOV ES:[SI], BX
         
-        MOV L4_HOLDER2, DI
-        MOV L5_HOLDER2, SI        
+        MOV LEN5_L4_HOLDER2[0], DI
+        MOV LEN5_L5_HOLDER2[0], SI        
         LOOP MOVE_21
     
-    DONE4:
+    DONE5:
+        CALL MOVE_UP
+        CALL MOVE_UP
+        CALL MOVE_UP
+        CALL MOVE_UP 
+        
+        JMP EXIT
+
+MOVE_3INWARDS:
+    MOV CX, 36 
+    MOV DI, 3840
+    MOV SI, 3842
+    MOV BP, 3844
+    PUSH DI
+    PUSH SI
+    PUSH BP
+
+    MOV DL, INPUT[2]
+    MOV DH, 00H
+    MOV ES:[DI], DX
+    MOV BL, INPUT[3]
+    MOV BH, 00H
+    MOV ES:[SI], BX
+    MOV AL, INPUT[4]
+    MOV AH, 00H
+    MOV ES:[BP], AX
+    
+    ADD DI, 2
+    MOV DL, INPUT[2]
+    MOV DH, 07H
+    MOV ES:[DI], DX    
+    ADD SI, 2
+    MOV BL, INPUT[3]
+    MOV BH, 07H
+    MOV ES:[SI], BX
+    ADD BP, 2
+    MOV BL, INPUT[4]
+    MOV BH, 07H
+    MOV ES:[BP], BX
+    
+    PUSH DI
+    PUSH SI
+    PUSH BP
+
+    MOVE_31:    
+        CMP CX, 0
+        JE DONE6
+        
+        POP BP
+        POP SI
+        POP DI
+        
+        MOV DL, INPUT[2]
+        MOV DH, 00H
+        MOV ES:[DI], DX
+        MOV BL, INPUT[3]
+        MOV BH, 00H
+        MOV ES:[SI], BX
+        MOV AL, INPUT[4]
+        MOV AH, 00H
+        MOV ES:[BP], AX
+            
+        ADD DI, 2
+        MOV DL, INPUT[2]
+        MOV DH, 07H
+        MOV ES:[DI], DX    
+        ADD SI, 2
+        MOV BL, INPUT[3]
+        MOV BH, 07H
+        MOV ES:[SI], BX
+        ADD BP, 2
+        MOV AL, INPUT[4]
+        MOV AH, 07H
+        MOV ES:[BP], AX
+        
+        PUSH DI
+        PUSH SI
+        PUSH BP
+        
+        MOV DI, LEN7_L5_HOLDER2
+        MOV SI, LEN7_L6_HOLDER2
+        MOV BP, LEN7_L7_HOLDER2
+
+        MOV DL, INPUT[6]
+        MOV DH, 00H
+        MOV ES:[DI], DX
+        MOV BL, INPUT[7]
+        MOV BH, 00H
+        MOV ES:[SI], BX
+        MOV AL, INPUT[8]
+        MOV AH, 00H
+        MOV ES:[BP], BX
+        
+        SUB DI, 2
+        MOV DL, INPUT[6]
+        MOV DH, 07H
+        MOV ES:[DI], DX    
+        SUB SI, 2
+        MOV BL, INPUT[7]
+        MOV BH, 07H
+        MOV ES:[SI], BX
+        SUB BP, 2
+        MOV AL, INPUT[8]
+        MOV AH, 07H
+        MOV ES:[BP], AX
+        
+        MOV LEN7_L5_HOLDER2, DI
+        MOV LEN7_L6_HOLDER2, SI
+        MOV LEN7_L7_HOLDER2, BP         
+        LOOP MOVE_31
+    
+    DONE6:
         CALL MOVE_UP
         CALL MOVE_UP
         CALL MOVE_UP
@@ -420,7 +655,15 @@ EXIT:
 MSG_PROMPT DB 'ENTER A ODD STRING: $'      
 INPUT DB 20, ?, 20 DUP(?)
 BLANK DB '                    '
-L4_HOLDER1 DW 82
-L5_HOLDER1 DW 84
-L4_HOLDER2 DW 3996
-L5_HOLDER2 DW 3998
+
+LEN5_L4_HOLDER1 DW 82
+LEN5_L5_HOLDER1 DW 84
+LEN5_L4_HOLDER2 DW 3996
+LEN5_L5_HOLDER2 DW 3998
+
+LEN7_L5_HOLDER1 DW 82
+LEN7_L6_HOLDER1 DW 84
+LEN7_L7_HOLDER1 DW 86
+LEN7_L5_HOLDER2 DW 3994
+LEN7_L6_HOLDER2 DW 3996
+LEN7_L7_HOLDER2 DW 3998
