4205d71b70feebdd7574fae74f6255fbd43af753
Sean Aguilar <seanaguilar698@gmail.com>
2024-09-23 12:39:39 +0800

    Create LE2

---
+++ LE2/activity2-2.asm
@@ -0,0 +1,14 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+    MOV SI, 02H
+    MOV DI, 04H
+    MOV AX, 0004H
+    MOV BX, [AX+SI+08]
+    MOV CX, 0000H
+    MOV DX, [AX+DI+08]    
+
+ret
